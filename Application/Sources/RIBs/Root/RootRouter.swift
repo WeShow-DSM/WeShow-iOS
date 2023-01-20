c4e32cfd4baa3f96a2b6aaa69807784313478fee
rlarldud1234 <rlarldud6482@naver.com>
2023-01-20 21:45:05 +0900

    feat :: Root RIBs Setting

---
+++ Application/Sources/RIBs/Root/RootRouter.swift
@@ -0,0 +1,18 @@
+import RIBs
+
+protocol RootInteractable: Interactable {
+    var router: RootRouting? { get set }
+    var listener: RootListener? { get set }
+}
+
+protocol RootViewControllable: ViewControllable {
+}
+
+final class RootRouter: LaunchRouter<RootInteractable, RootViewControllable>, RootRouting {
+
+    override init(interactor: RootInteractable, viewController: RootViewControllable) {
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
+
+}
