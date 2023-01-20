5c9dfe5fdb232b0a82dcb59e3f12ff7992152f52
rlarldud1234 <rlarldud6482@naver.com>
2023-01-20 21:45:19 +0900

    feat :: SceneDelegate App 세팅함

--- Application/Sources/Application/SceneDelegate.swift
+++ Application/Sources/Application/SceneDelegate.swift
@@ -1,19 +1,25 @@
 import UIKit
+import RIBs
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+
+    private var launchRouter: LaunchRouting?
 
     func scene(
         _ scene: UIScene,
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: UIScreen.main.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = UIViewController()
-        window?.makeKeyAndVisible()
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            self.window = window
+
+            let launchRouter = RootBuilder(dependency: AppComponent()).build()
+            self.launchRouter = launchRouter
+            launchRouter.launch(from: window)
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
