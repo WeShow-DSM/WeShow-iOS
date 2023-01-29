9263db7658d47912f439ef361c0a77df6af671a6
rlarldud1234 <rlarldud6482@naver.com>
2023-01-29 22:37:11 +0900

    feat :: Root -> TabBarRIBs를 attachChild함

--- Application/Sources/RIBs/Root/RootRouter.swift
+++ Application/Sources/RIBs/Root/RootRouter.swift
@@ -1,18 +1,40 @@
 import RIBs
 
-protocol RootInteractable: Interactable {
+protocol RootInteractable: Interactable, TabBarListener {
     var router: RootRouting? { get set }
     var listener: RootListener? { get set }
 }
 
 protocol RootViewControllable: ViewControllable {
+    func present(viewController: ViewControllable)
 }
 
 final class RootRouter: LaunchRouter<RootInteractable, RootViewControllable>, RootRouting {
 
-    override init(interactor: RootInteractable, viewController: RootViewControllable) {
+    private let tabBarBuilder: TabBarBuildable
+
+    private var tabBar: ViewableRouting?
+
+    init(
+        interactor: RootInteractable,
+        viewController: RootViewControllable,
+        tabBarBuilder: TabBarBuildable
+    ) {
+        self.tabBarBuilder = tabBarBuilder
         super.init(interactor: interactor, viewController: viewController)
         interactor.router = self
+    }
+
+    override func didLoad() {
+        super.didLoad()
+        attachTabBar()
+    }
+
+    private func attachTabBar() {
+        let tabBar = tabBarBuilder.build(withListener: interactor)
+        self.tabBar = tabBar
+        attachChild(tabBar)
+        viewController.present(viewController: tabBar.viewControllable)
     }
 
 }
