ed7f9f9a7780384144355a6436f2cee6d5eea951
rlarldud1234 <rlarldud6482@naver.com>
2023-01-29 22:36:07 +0900

    feat :: LaunchScreen 설정해주기

--- Application/Sources/Application/AppDelegate.swift
+++ Application/Sources/Application/AppDelegate.swift
@@ -7,7 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
-        // Override point for customization after application launch.
+        Thread.sleep(forTimeInterval: 1)
         return true
     }
 
