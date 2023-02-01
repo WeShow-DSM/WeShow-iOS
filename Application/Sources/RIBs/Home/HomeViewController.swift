95a2a18a37d71c17a214a0d414fd533e54d825f0
rlarldud1234 <rlarldud6482@naver.com>
2023-02-01 21:25:31 +0900

    feat :: UI 일단 커밋

--- Application/Sources/RIBs/Home/HomeViewController.swift
+++ Application/Sources/RIBs/Home/HomeViewController.swift
@@ -1,6 +1,9 @@
 import RIBs
 import RxSwift
 import UIKit
+import FlexLayout
+import PinLayout
+import Then
 
 protocol HomePresentableListener: AnyObject {
 }
@@ -9,8 +12,16 @@ final class HomeViewController: UIViewController, HomePresentable, HomeViewContr
 
     weak var listener: HomePresentableListener?
 
+    private let rootFlexContainer = UIView()
+
+    private let searchTextField = UITextField().then {
+        $0.placeholder = "상품을 검색해주세요..."
+        $0.font = .systemFont(ofSize: 16, weight: .light)
+        $0.addImageToLeft(.init(systemName: "magnifyingglass") ?? UIImage())
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        view.backgroundColor = .white
     }
 }
