ff43f72437820c326f2c544cec8b0c5b5b386832
rlarldud1234 <rlarldud6482@naver.com>
2023-02-01 21:25:05 +0900

    feat :: UITextField + addPaddingToLeft

---
+++ Application/Sources/Extension/UITextField+addPaddingToLeft.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+extension UITextField {
+    func addPaddingToLeft() {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+        self.leftView = paddingView
+        self.leftViewMode = ViewMode.always
+    }
+}
