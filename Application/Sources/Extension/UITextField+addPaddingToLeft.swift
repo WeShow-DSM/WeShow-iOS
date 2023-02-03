05526157e21529ee03dec5a07c3ddeed656d0a0d
rlarldud1234 <rlarldud6482@naver.com>
2023-02-03 11:28:58 +0900

    delete :: 파일 이동으로 인한 파일 삭제

--- Application/Sources/Extension/UITextField+addPaddingToLeft.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension UITextField {
-    func addPaddingToLeft() {
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
-        self.leftView = paddingView
-        self.leftViewMode = ViewMode.always
-    }
-}
