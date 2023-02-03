aabf3ccf8998690cfd0648374db1cbd335f2ad68
rlarldud1234 <rlarldud6482@naver.com>
2023-02-03 11:28:26 +0900

    delet :: 파일 삭제 (tuist asset 코드 가지고 코딩하면 됨!)

--- Application/Sources/Extension/UIColor/UIColor+Point.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-extension UIColor {
-    static let pointMinus2 = UIColor(named: "Point-2")
-    static let pointMinus1 = UIColor(named: "Point-1")
-    static let point = UIColor(named: "Point")
-    static let pointPlus1 = UIColor(named: "Point+1")
-    static let pointPlus2 = UIColor(named: "Point+2")
-}
