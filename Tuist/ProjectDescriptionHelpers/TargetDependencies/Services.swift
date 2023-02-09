28964a5428399050ab3b19ff95ef9ad8ff96cfb1
rlarldud1234 <rlarldud6482@naver.com>
2023-02-09 13:31:28 +0900

    feat :: TargetDependency 설정하기

---
+++ Tuist/ProjectDescriptionHelpers/TargetDependencies/Services.swift
@@ -0,0 +1,16 @@
+import ProjectDescription
+
+extension TargetDependency {
+
+    public struct Services {
+        public static let WeShowService = service(name: "WeShowService")
+        public static let AuthService = service(name: "AuthService")
+    }
+
+    private static func service(name: String) -> TargetDependency {
+        return TargetDependency.project(
+            target: name,
+            path: .relativeToRoot("Services/\(name)")
+        )
+    }
+}
