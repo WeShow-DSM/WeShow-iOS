af04a4cb736a1d5b6089d38fab73bc0aef274d82
rlarldud1234 <rlarldud6482@naver.com>
2023-02-14 11:35:49 +0900

    feat :: CategoryViewController Publishing

--- Application/Sources/Presentation/RIBs/Category/CategoryViewController.swift
+++ Application/Sources/Presentation/RIBs/Category/CategoryViewController.swift
@@ -2,6 +2,10 @@ import RIBs
 import RxSwift
 import UIKit
 
+import FlexLayout
+import PinLayout
+import Then
+
 protocol CategoryPresentableListener: AnyObject {
 }
 
@@ -9,8 +13,65 @@ final class CategoryViewController: UIViewController, CategoryPresentable, Categ
 
     weak var listener: CategoryPresentableListener?
 
+    private let rootFlexContainer = UIView()
+    private let categoryImageStringList = ["feed", "toy", "walkingProduct",
+                                           "sanitaryProduct", "electronicDevices", "beauty",
+                                           "clothing", "interior", "toiletProduct", ""]
+    private let categoryTitleList = ["사료, 간식", "장난감", "산책용품", "위생용품", "전자기기",
+                                     "미용 & 청결", "의류", "홈인테리어", "배변용품", "기타"]
+
+    // MARK: - UI
+    private let categoryTableView = UITableView().then {
+        $0.backgroundColor = .clear
+        $0.register(CategoryTableViewCell.self, forCellReuseIdentifier: "cell")
+        $0.separatorStyle = .none
+    }
+
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .orange
+        view.backgroundColor = .init(asset: WeShowIOSAsset.Color.gray100)
+        self.setTableView()
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setupLayoutWithFlex()
+        setupRootFlexContainer()
+    }
+}
+
+// MARK: - Layout
+extension CategoryViewController {
+    private func setupRootFlexContainer() {
+        self.view.addSubview(rootFlexContainer)
+        self.rootFlexContainer.pin.all(view.pin.safeArea)
+        self.rootFlexContainer.flex.layout()
+    }
+    private func setupLayoutWithFlex() {
+        self.rootFlexContainer.flex.define { flex in
+            flex.addItem(categoryTableView).position(.absolute).all(0).marginHorizontal(24)
+        }
+    }
+}
+
+// MARK: - TableView
+extension CategoryViewController: UITableViewDelegate, UITableViewDataSource {
+    private func setTableView() {
+        self.categoryTableView.delegate = self
+        self.categoryTableView.dataSource = self
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as? CategoryTableViewCell
+        cell?.setData(
+            categoryImageString: categoryImageStringList[indexPath.row],
+            categoryTitle: categoryTitleList[indexPath.row]
+        )
+
+        return cell ?? UITableViewCell()
     }
 }
