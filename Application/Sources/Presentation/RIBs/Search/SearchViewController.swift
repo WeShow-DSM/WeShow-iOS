021b2003749ea9dec71c2cd31efb697fb87e9d82
김대희 <0824dh@naver.com>
2023-02-14 21:32:13 +0900

    feat :: search 하위 뷰 addchild 하기

--- Application/Sources/Presentation/RIBs/Search/SearchViewController.swift
+++ Application/Sources/Presentation/RIBs/Search/SearchViewController.swift
@@ -9,6 +9,7 @@ protocol SearchPresentableListener: AnyObject {
 final class SearchViewController: UIViewController, SearchPresentable, SearchViewControllable {
 
     weak var listener: SearchPresentableListener?
+    var childrens: [UIViewController] = []
     private let rootFlexContainer = UIView()
 
     // MARK: - SearchBar
@@ -33,9 +34,14 @@ final class SearchViewController: UIViewController, SearchPresentable, SearchVie
         $0.showsVerticalScrollIndicator = false
     }
 
+    private let searchContainerView = UIView()
+    private let recentSearchView = RecentSearchView()
+    private let searchResultView = SearchResultView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .init(asset: WeShowIOSAsset.Color.gray100)
+        searchContainerView.backgroundColor = .init(asset: WeShowIOSAsset.Color.point)
     }
 
     override func viewWillLayoutSubviews() {
@@ -48,20 +54,31 @@ final class SearchViewController: UIViewController, SearchPresentable, SearchVie
 // MARK: - Layout
 extension SearchViewController {
     private func setupRootFlexContainer() {
-        self.view.addSubview(scrollView)
-        scrollView.addSubview(rootFlexContainer)
-        self.scrollView.pin.all(view.pin.safeArea)
-        self.rootFlexContainer.pin.top().left().right()
+        self.view.addSubview(rootFlexContainer)
+        self.rootFlexContainer.pin.all(view.pin.safeArea)
         self.rootFlexContainer.flex.layout(mode: .adjustHeight)
-        self.scrollView.contentSize = rootFlexContainer.frame.size
+
+        [searchResultView, recentSearchView].forEach {
+            addChild($0)
+        }
+
+        recentSearchView.didMove(toParent: self)
     }
 
     private func setupLayoutWithFlex() {
+        print(self.rootFlexContainer.frame)
         self.rootFlexContainer.flex.define { flex in
-            flex.addItem(searchBackView).marginTop(15).height(50).marginHorizontal(24).direction(.row).define {flex in
+            flex.addItem(searchBackView).marginTop(15).height(50).marginHorizontal(24).direction(.row).define { flex in
                 flex.addItem(searchTextField).marginLeft(15).marginVertical(10).marginRight(10).grow(1)
                 flex.addItem(searchButton).marginRight(10).marginVertical(10).height(30)
             }
+            flex.addItem(searchContainerView)
+                .marginHorizontal(0)
+                .height(self.view.safeAreaLayoutGuide.layoutFrame.height - 65)
+                .define { flex in
+                    flex.addItem(searchResultView.view).position(.absolute).all(0)
+                    flex.addItem(recentSearchView.view).position(.absolute).all(0)
+                }
         }
     }
 }
