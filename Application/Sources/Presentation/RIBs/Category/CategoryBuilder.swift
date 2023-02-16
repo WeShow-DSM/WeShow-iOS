7a594db39fc32f269172c98ac4227a519a140f78
rlarldud1234 <rlarldud6482@naver.com>
2023-02-16 16:07:03 +0900

    feat :: ReactorKit 적용

--- Application/Sources/Presentation/RIBs/Category/CategoryBuilder.swift
+++ Application/Sources/Presentation/RIBs/Category/CategoryBuilder.swift
@@ -3,26 +3,35 @@ import RIBs
 protocol CategoryDependency: Dependency {
 }
 
-final class CategoryComponent: Component<CategoryDependency> {
+final class CategoryComponent: Component<CategoryDependency>, DetailCategoryDependency {
 }
 
 // MARK: - Builder
-
 protocol CategoryBuildable: Buildable {
     func build(withListener listener: CategoryListener) -> CategoryRouting
 }
 
 final class CategoryBuilder: Builder<CategoryDependency>, CategoryBuildable {
 
+    var initialState = CategoryPresentableState()
+
     override init(dependency: CategoryDependency) {
         super.init(dependency: dependency)
     }
 
     func build(withListener listener: CategoryListener) -> CategoryRouting {
-        _ = CategoryComponent(dependency: dependency)
+        let component = CategoryComponent(dependency: dependency)
         let viewController = CategoryViewController()
-        let interactor = CategoryInteractor(presenter: viewController)
+        let detailCategoryBuilder = DetailCategoryBuilder(dependency: component)
+        let interactor = CategoryInteractor(
+            presenter: viewController,
+            initialState: initialState
+        )
         interactor.listener = listener
-        return CategoryRouter(interactor: interactor, viewController: viewController)
+        return CategoryRouter(
+            detailCategoryBuilder: detailCategoryBuilder,
+            interactor: interactor,
+            viewController: viewController
+        )
     }
 }
