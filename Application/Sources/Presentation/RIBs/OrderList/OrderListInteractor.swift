affc3de9bbe58bd44b59f0b4a2f8d08bca47fec5
rlarldud1234 <rlarldud6482@naver.com>
2023-02-24 15:45:31 +0900

    feat :: 화면 이동 설정해주기

--- Application/Sources/Presentation/RIBs/OrderList/OrderListInteractor.swift
+++ Application/Sources/Presentation/RIBs/OrderList/OrderListInteractor.swift
@@ -1,7 +1,10 @@
 import RIBs
+import ReactorKit
 import RxSwift
 
 protocol OrderListRouting: ViewableRouting {
+    func attachDelieveryCheckRIB()
+    func detachRIB()
 }
 
 protocol OrderListPresentable: Presentable {
@@ -11,12 +14,26 @@ protocol OrderListPresentable: Presentable {
 protocol OrderListListener: AnyObject {
 }
 
-final class OrderListInteractor: PresentableInteractor<OrderListPresentable>, OrderListInteractable, OrderListPresentableListener {
+// swiftlint:disable line_length
+final class OrderListInteractor: PresentableInteractor<OrderListPresentable>, OrderListInteractable, OrderListPresentableListener, Reactor {
+
+    typealias Action = OrderListPresentableAction
+    typealias State = OrderListPresentableState
+
+    enum Mutation {
+        case attachDeliveryCheckRIB
+    }
 
     weak var router: OrderListRouting?
     weak var listener: OrderListListener?
 
-    override init(presenter: OrderListPresentable) {
+    let initialState: OrderListPresentableState
+
+    init(
+        initialState: OrderListPresentableState,
+        presenter: OrderListPresentable
+    ) {
+        self.initialState = initialState
         super.init(presenter: presenter)
         presenter.listener = self
     }
@@ -27,5 +44,33 @@ final class OrderListInteractor: PresentableInteractor<OrderListPresentable>, Or
 
     override func willResignActive() {
         super.willResignActive()
+    }
+
+    func sendAction(_ action: Action) {
+        self.action.on(.next(action))
+    }
+}
+
+// MARK: - mutate
+extension OrderListInteractor {
+    func mutate(action: OrderListPresentableAction) -> Observable<Mutation> {
+        switch action {
+        case .deliveryCheckButtonDidTap:
+            return attachCheckDeliveryRIBMutation()
+        default:
+            return detachRIBMutation()
+        }
+    }
+}
+
+// MARK: - Mutate
+extension OrderListInteractor {
+    private func attachCheckDeliveryRIBMutation() -> Observable<Mutation> {
+        self.router?.attachDelieveryCheckRIB()
+        return .just(.attachDeliveryCheckRIB)
+    }
+    private func detachRIBMutation() -> Observable<Mutation> {
+        self.router?.detachRIB()
+        return .empty()
     }
 }
--- Application/Sources/Presentation/RIBs/OrderList/OrderListRouter.swift
+++ Application/Sources/Presentation/RIBs/OrderList/OrderListRouter.swift
@@ -1,6 +1,6 @@
 import RIBs
 
-protocol OrderListInteractable: Interactable {
+protocol OrderListInteractable: Interactable, CheckDeliveryListener {
     var router: OrderListRouting? { get set }
     var listener: OrderListListener? { get set }
 }
@@ -10,8 +10,30 @@ protocol OrderListViewControllable: ViewControllable {
 
 final class OrderListRouter: ViewableRouter<OrderListInteractable, OrderListViewControllable>, OrderListRouting {
 
-    override init(interactor: OrderListInteractable, viewController: OrderListViewControllable) {
+    private let checkDeliveryBuilder: CheckDeliveryBuildable
+    private let checkDeliveryRouter: CheckDeliveryRouting
+    private var currentRouter: Routing?
+
+    init(
+        checkDeliveryBuilder: CheckDeliveryBuildable,
+        interactor: OrderListInteractable,
+        viewController: OrderListViewControllable
+    ) {
+        self.checkDeliveryBuilder = checkDeliveryBuilder
+        self.checkDeliveryRouter = checkDeliveryBuilder.build(withListener: interactor)
         super.init(interactor: interactor, viewController: viewController)
         interactor.router = self
+    }
+
+    func attachDelieveryCheckRIB() {
+        guard currentRouter == nil else { return }
+        let deliveryCheckViewController = checkDeliveryRouter.viewControllable
+        self.viewController.pushViewController(viewController: deliveryCheckViewController)
+        self.currentRouter = checkDeliveryRouter
+    }
+    func detachRIB() {
+        guard let router = currentRouter else { return }
+        self.currentRouter = nil
+        self.detachChild(router)
     }
 }
