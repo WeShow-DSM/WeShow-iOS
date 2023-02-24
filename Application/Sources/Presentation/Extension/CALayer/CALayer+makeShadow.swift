59d5213f13fe5325890bda4912a020eba6707a2d
rlarldud1234 <rlarldud6482@naver.com>
2023-02-24 15:43:57 +0900

    feat :: Shadow가 너무 진해서 많이 낮춤

--- Application/Sources/Presentation/Extension/CALayer/CALayer+makeShadow.swift
+++ Application/Sources/Presentation/Extension/CALayer/CALayer+makeShadow.swift
@@ -3,7 +3,7 @@ import UIKit
 extension CALayer {
     func makeShadow() {
         self.shadowColor = UIColor(asset: WeShowIOSAsset.Color.pointMinus2)?.cgColor
-        self.shadowOpacity = 0.06
+        self.shadowOpacity = 0.006
         self.shadowRadius = 30
         self.shadowOffset = .init(width: 0, height: 2)
     }
