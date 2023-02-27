dc3e16666d108f156b5601d57425ca8d6649170e
rlarldud1234 <rlarldud6482@naver.com>
2023-02-27 13:05:35 +0900

    feat :: UITextField leftPadding width 15로 변경

--- Application/Sources/Presentation/Extension/UITextField/UITextField+addPaddingToLeft.swift
+++ Application/Sources/Presentation/Extension/UITextField/UITextField+addPaddingToLeft.swift
@@ -2,7 +2,7 @@ import UIKit
 
 extension UITextField {
     func addPaddingToLeft() {
-        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: self.frame.height))
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: self.frame.height))
         self.leftView = paddingView
         self.leftViewMode = ViewMode.always
     }
