4a183595af624375761485bec3b08176487ca4ef
rlarldud1234 <rlarldud6482@naver.com>
2023-02-27 13:06:15 +0900

    feat :: LoginRIB 추가 및 퍼블리싱

---
+++ Application/Sources/Presentation/RIBs/Login/LoginBuilder.swift
@@ -0,0 +1,27 @@
+import RIBs
+
+protocol LoginDependency: Dependency {
+}
+
+final class LoginComponent: Component<LoginDependency> {
+}
+
+// MARK: - Builder
+protocol LoginBuildable: Buildable {
+    func build(withListener listener: LoginListener) -> LoginRouting
+}
+
+final class LoginBuilder: Builder<LoginDependency>, LoginBuildable {
+
+    override init(dependency: LoginDependency) {
+        super.init(dependency: dependency)
+    }
+
+    func build(withListener listener: LoginListener) -> LoginRouting {
+        let component = LoginComponent(dependency: dependency)
+        let viewController = LoginViewController()
+        let interactor = LoginInteractor(presenter: viewController)
+        interactor.listener = listener
+        return LoginRouter(interactor: interactor, viewController: viewController)
+    }
+}
